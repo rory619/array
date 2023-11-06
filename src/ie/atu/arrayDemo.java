4d8f0a83626170b531b77dac2cd7313fca94892f
Rory <G00422319@atu.ie>
2023-11-06 10:04:20 +0000

    committing files for array project, made an array demo

---
+++ src/ie/atu/arrayDemo.java
@@ -0,0 +1,34 @@
+package ie.atu;
+
+
+public class arrayDemo {
+    public static void main(String[] args) {
+        //declares an array of integers
+        int[] anArray;
+
+        //allocates memory for 10 integers
+        anArray = new int[10];
+
+        //initialise first element
+        anArray[0] = 100;
+        //initialise second element
+        anArray[1] = 200;
+        anArray[2] = 300;
+        anArray[3] = 400;
+        anArray[4] = 500;
+        anArray[5] = 600;
+        anArray[6] = 700;
+        anArray[7] = 800;
+        anArray[8] = 900;
+        anArray[9] = 1000;
+
+        int indexValue = 0;
+        for (int value: anArray)
+        {
+            System.out.println("Element at index 0:" + indexValue + value);
+            indexValue++;
+        }
+
+
+    }
+}
